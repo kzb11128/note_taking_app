37db2572afba307e6ce30b40e0f4628e3c50c7f4
Zhang <kzb11128@gmail.com>
2023-06-17 16:12:52 -0400

    fix files

--- server.js
+++ server.js
@@ -1,11 +1,23 @@
 const express = require('express');
+const path = require('path');
 const app = express();
-const port = process.env.PORT || 3001;
-const api = require('./public/assets/js/index.js');
+const PORT = process.env.PORT || 3001;
+// const api = require('./assets/js/index.js');
 
-app.use(express.static('public'));
-app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-app.use('/api', api);
+app.use(express.urlencoded({ extended: true }));
+// app.use('/api', api);
+app.use(express.static('public'));
 
+app.get(['/', '/index'], (req, res) =>
+  res.sendFile(path.join(__dirname, '/public/index.html'))
+);
+
+app.get('/notes', (req, res) =>
+  res.sendFile(path.join(__dirname, '/public/notes.html'))
+);
+
+app.listen(PORT, () => 
+console.log(`Listening on at http://localhost:${PORT}`)
+);
 
