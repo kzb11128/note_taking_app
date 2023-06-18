15980e0b0c9a1f6cc3fd67b564309c74c9327a46
Zhang <kzb11128@gmail.com>
2023-06-17 21:14:03 -0400

    removed manual GET for index.htlm

--- server.js
+++ server.js
@@ -9,10 +9,6 @@ app.use(express.urlencoded({ extended: true }));
 // app.use('/api', api);
 app.use(express.static('public'));
 
-app.get(['/', '/index'], (req, res) =>
-  res.sendFile(path.join(__dirname, '/public/index.html'))
-);
-
 app.get('/notes', (req, res) =>
   res.sendFile(path.join(__dirname, '/public/notes.html'))
 );
